82cdd64132db9494e53f10e127ba4dcd8874e0b1
xero <x@xero.nu>
2012-03-20 14:41:56 -0400

    updated xbd.php - added new ip / hostname lookup feature, added android phone detection, and wrapped the file into a class.

--- xbd.php
+++ xbd.php
@@ -1,7 +1,10 @@
 <?php
 /*
+$Id: v 1.402 2012/03/20 08:06:00 $
+
 <XBD, Extended Browser Detection.>
 Copyright (C) <2009>  <Guillermo Azurdia, www.nopticon.com>
+Forked    (F) <2012>  <xero harrison, xero.nu>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,15 +18,18 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
-function _print($name, $ret = false)
-{
-	echo $name . '<br />';
-	
-	if ($ret !== false)
+forked by xero harrison for use in the open qoob framework.
+by forked i mean: wrapped into a class, added android detection
+and ip / hostname lookup.
+
+*/
+class xbd {
+	function _print($name, $ret)
 	{
-		if (!f($ret))
+		echo $name . '<br />';
+		
+		if (!$this->f($ret))
 		{
 			$ret = 'false';
 		}
@@ -35,189 +41,197 @@ function _print($name, $ret = false)
 		echo '<blockquote><pre>';
 		print_r($ret);
 		echo '</pre></blockquote>';
-	}
-	
-	return;
-}
-
-function v_server($a)
-{
-	return (isset($_SERVER[$a])) ? $_SERVER[$a] : '';
-}
-
-function f($s)
-{
-	return !empty($s);
-}
-
-function w($a = '', $d = false)
-{
-	if (!f($a) || !is_string($a)) return array();
-	
-	$e = explode(' ', $a);
-	if ($d !== false)
-	{
-		foreach ($e as $i => $v)
-		{
-			$e[$v] = $d;
-			unset($e[$i]);
-		}
-	}
-	
-	return $e;
-}
-
-/*
-If you want to add more browsers, do it but be careful... order matters.
-*/
-function _browser($a_browser = false, $a_version = false, $name = false, $d_name = false, $ret_ary = false)
-{
-	$browser_list  = 'nokia motorola samsung sonyericsson blackberry iphone htc ';
-	$browser_list .= 'flock firefox konqueror lobo msie netscape navigator mosaic netsurf lynx amaya omniweb ';
-	$browser_list .= 'googlebot googlebot-image feedfetcher-google gigabot msnbot thunderbird fennec minimo ';
-	$browser_list .= 'minefield chrome wget cheshire safari avant camino seamonkey aol bloglines ';
-	$browser_list .= 'wii playstation netfront opera mozilla gecko ubuntu';
-	
-	$browser_type = array(
-		'mobile' => 'nokia motorola samsung sonyericsson blackberry iphone fennec minimo htc',
-		'console' => 'wii playstation',
-		'bot' => 'googlebot googlebot-image feedfetcher-google gigabot msnbot bloglines'
-	);
-	
-	$platforms = array(
-		'linux' => w('linux'),
-		'mac' => array('macintosh', 'mac platform x', 'mac os x'),
-		'windows' => w('windows win32')
-	);
-	
-	$user_browser = strtolower(v_server('HTTP_USER_AGENT'));
-	
-	$this_version = $this_browser = $this_platform = '';
-	
-	if ($a_browser == '*') {
-		$a_browser = $a_version = $name = false;
-		$d_name = true;
-	}
-	
-	if ($a_browser === false && $a_version === false && $name === false && $d_name !== false)
-	{
-		return $user_browser;
-	}
-	
-	foreach (w('user_browser a_browser a_version name d_name') as $row)
-	{
-		$vrow = $$row;
-		if (is_string($vrow)) {
-			$$row = strtolower($vrow);
-		}
-	}
-	
-	$browser_limit = strlen($user_browser);
-	foreach (w($browser_list) as $row)
-	{
-		$row = ($a_browser !== false) ? $a_browser : $row;
-		$n = stristr($user_browser, $row);
-		if (!$n || f($this_browser)) continue;
 		
-		$this_browser = $row;
-		$j = strpos($user_browser, $row) + strlen($row);
-		$j2 = substr($user_browser, $j, 1);
-		if (preg_match('#[\/\_\-\ ]#', $j2)) {
-			$j += 1;
-		}
-		
-		for (; $j <= $browser_limit; $j++)
-		{
-			$s = trim(substr($user_browser, $j, 1));
-			if (!preg_match('/[\w\.\-]/', $s)) break;
-			
-			$this_version .= $s;
-		}
+		return;
 	}
 	
-	if ($a_browser !== false && ($d_name === false || $name === true) && $ret_ary === false)
+	function v_server($a)
 	{
-		$ret = false;
-		if (strtolower($a_browser) == $this_browser)
+		return (isset($_SERVER[$a])) ? $_SERVER[$a] : '';
+	}
+	
+	function f($s)
+	{
+		return !empty($s);
+	}
+	
+	function w($a = '', $d = false)
+	{
+		if (!$this->f($a) || !is_string($a)) return array();
+		
+		$e = explode(' ', $a);
+		if ($d !== false)
 		{
-			$ret = true;
-			if ($a_version !== false)
+			foreach ($e as $i => $v)
 			{
-				if (f($this_version))
+				$e[$v] = $d;
+				unset($e[$i]);
+			}
+		}
+		
+		return $e;
+	}
+	/*
+	If you want to add more browsers, do it but be careful... order matters.
+	*/
+	function browser($a_browser = false, $a_version = false, $name = false, $d_name = false, $ret_ary = false)
+	{
+		//---added android [03.20.12]
+		$browser_list  = 'nokia motorola samsung sonyericsson blackberry iphone htc android ';
+		$browser_list .= 'flock firefox konqueror lobo msie netscape navigator mosaic netsurf lynx amaya omniweb ';
+		$browser_list .= 'googlebot googlebot-image feedfetcher-google gigabot msnbot thunderbird fennec minimo ';
+		$browser_list .= 'minefield chrome wget cheshire safari avant camino seamonkey aol bloglines ';
+		$browser_list .= 'wii playstation netfront opera mozilla gecko ubuntu';
+		
+		$browser_type = array(
+			'mobile' => 'nokia motorola samsung sonyericsson blackberry iphone fennec minimo htc android',
+			'console' => 'wii playstation',
+			'bot' => 'googlebot googlebot-image feedfetcher-google gigabot msnbot bloglines'
+		);
+		
+		$platforms = array(
+			'linux' => $this->w('linux'),
+			'mac' => array('macintosh', 'mac platform x', 'mac os x'),
+			'windows' => $this->w('windows win32')
+		);
+		
+		$user_browser = strtolower($this->v_server('HTTP_USER_AGENT'));
+		
+		$this_version = $this_browser = $this_platform = '';
+		
+		if ($a_browser == '*') {
+			$a_browser = $a_version = $name = false;
+			$d_name = true;
+		}
+		
+		if ($a_browser === false && $a_version === false && $name === false && $d_name !== false)
+		{
+			return $user_browser;
+		}
+		
+		foreach ($this->w('user_browser a_browser a_version name d_name') as $row)
+		{
+			$vrow = $$row;
+			if (is_string($vrow)) {
+				$$row = strtolower($vrow);
+			}
+		}
+		
+		$browser_limit = strlen($user_browser);
+		foreach ($this->w($browser_list) as $row)
+		{
+			$row = ($a_browser !== false) ? $a_browser : $row;
+			$n = stristr($user_browser, $row);
+			if (!$n || $this->f($this_browser)) continue;
+			
+			$this_browser = $row;
+			$j = strpos($user_browser, $row) + strlen($row);
+			$j2 = substr($user_browser, $j, 1);
+			if (preg_match('#[\/\_\-\ ]#', $j2)) {
+				$j += 1;
+			}
+			
+			for (; $j <= $browser_limit; $j++)
+			{
+				$s = trim(substr($user_browser, $j, 1));
+				if (!preg_match('/[\w\.\-]/', $s)) break;
+				
+				$this_version .= $s;
+			}
+		}
+		
+		if ($a_browser !== false && ($d_name === false || $name === true) && $ret_ary === false)
+		{
+			$ret = false;
+			if (strtolower($a_browser) == $this_browser)
+			{
+				$ret = true;
+				if ($a_version !== false)
 				{
-					$a_sign = explode(' ', $a_version);
-					if (version_compare($this_version, $a_sign[1], $a_sign[0]) === false) {
+					if ($this->f($this_version))
+					{
+						$a_sign = explode(' ', $a_version);
+						if (version_compare($this_version, $a_sign[1], $a_sign[0]) === false) {
+							$ret = false;
+							$vf = true;
+						}
+					}
+					else
+					{
 						$ret = false;
-						$vf = true;
 					}
 				}
-				else
-				{
-					$ret = false;
-				}
+			}
+			
+			if ($name !== true)
+			{
+				return $ret;
 			}
 		}
 		
-		if ($name !== true)
+		foreach ($platforms as $os => $match)
 		{
-			return $ret;
-		}
-	}
-	
-	foreach ($platforms as $os => $match)
-	{
-		foreach ($match as $os_name)
-		{
-			if (strpos($user_browser, $os_name) !== false)
+			foreach ($match as $os_name)
 			{
-				$this_platform = $os;
-				break 2;
-			}
-		}
-	}
-	
-	$this_type = '';
-	if (f($this_browser))
-	{
-		foreach ($browser_type as $type => $browsers)
-		{
-			foreach (w($browsers) as $row)
-			{
-				if (strpos($this_browser, $row) !== false)
+				if (strpos($user_browser, $os_name) !== false)
 				{
-					$this_type = $type;
+					$this_platform = $os;
 					break 2;
 				}
 			}
 		}
 		
-		if (!$this_type) $this_type = 'desktop';
-	}
-	
-	if ($name !== false && $ret_ary === false)
-	{
-		if ($a_browser !== false && $a_version !== false && $ret === false)
+		$this_type = '';
+		if ($this->f($this_browser))
 		{
-			return false;
+			foreach ($browser_type as $type => $browsers)
+			{
+				foreach ($this->w($browsers) as $row)
+				{
+					if (strpos($this_browser, $row) !== false)
+					{
+						$this_type = $type;
+						break 2;
+					}
+				}
+			}
+			
+			if (!$this_type) $this_type = 'desktop';
 		}
 		
-		$s_browser = '';
-		$s_data = array($this_type, $this_platform, $this_browser, $this_version);
-		foreach ($s_data as $row)
+		if ($name !== false && $ret_ary === false)
 		{
-			if (f($row)) $s_browser .= (($s_browser != '') ? ' ' : '') . $row;
+			if ($a_browser !== false && $a_version !== false && $ret === false)
+			{
+				return false;
+			}
+			
+			$s_browser = '';
+			$s_data = array($this_type, $this_platform, $this_browser, $this_version);
+			foreach ($s_data as $row)
+			{
+				if ($this->f($row)) $s_browser .= (($s_browser != '') ? ' ' : '') . $row;
+			}
+			
+			return $s_browser;
 		}
-		
-		return $s_browser;
+		// --- added by xero [04.02.10]
+		$addr = $_SERVER['REMOTE_ADDR'];
+		$host = @gethostbyaddr($addr);
+		if($host == '') {
+			$host = "unknown";
+		} 
+
+		return array(
+			'browser' => $this_browser,
+			'version' => $this_version,
+			'platform' => $this_platform,
+			'type' => $this_type,
+			'useragent' => $user_browser,
+			'ipaddress' => $addr,
+			'hostname' => $host
+		);
 	}
-	
-	return array(
-		'browser' => $this_browser,
-		'version' => $this_version,
-		'platform' => $this_platform,
-		'type' => $this_type,
-		'useragent' => $user_browser
-	);
 }
 
 ?>
