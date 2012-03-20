cc83973ad48637dc383a19acf580664e9ba7f10c
xero <x@xero.nu>
2012-03-20 14:44:33 -0400

    created new example.php file. this version focuses on the data returned from the browser function. see nopticon example file for an overview of other features.

--- example.php
+++ example.php
@@ -1,7 +1,10 @@
 <?php
 /*
+$Id: v 1.402 2012/03/20 08:06:00 $
+
 <XBD, Extended Browser Detection.>
 Copyright (C) <2009>  <Guillermo Azurdia, www.nopticon.com>
+Forked    (F) <2012>  <xero harrison, xero.nu>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,9 +18,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
-require_once('./xbd.php');
+forked by xero harrison for use in the open qoob framework.
+by forked i mean: wrapped into a class, added android detection
+and ip / hostname lookup.
+https://github.com/fontvirus/xbd
+*/
 
 // -----------------------------------------------------------------------------------
 // NOTES:
@@ -44,131 +50,129 @@ require_once('./xbd.php');
 // a new argument to return array data based on first argument.
 // -----------------------------------------------------------------------------------
 
-// Return array of browser data
-$ret = _browser();
-_print('All', $ret);
-
-$ret = _browser('*');
-_print('Useragent string', $ret);
-
-// Return true if using firefox
-$ret = _browser('firefox');
-_print('Firefox', $ret);
-
-// Return true if using IE 7.0 or above else false
-$ret = _browser('msie', '>= 7.0');
-_print('MSIE >= 7.0', $ret);
-
-// Return true if using below firefox 3.0.5 (can check minor version)
-$ret = _browser('firefox', '< 3.0.5');
-_print('Firefox < 3.0.5', $ret);
-
-// Return string of name of browser and version
-$ret = _browser(false, false, true);
-_print('Browser and version', $ret);
-
-// To check if Gecko browser is used
-$ret = _browser('gecko');
-_print('Is Gecko?', $ret);
-
-$ret = _browser('firefox', 'le 1.5');
-_print('Operator by name (Checking Firefox le 1.5)', $ret);
-
-$ret = _browser('firefox', 'ge 3.5.1');
-_print('Operator by name (Checking Firefox ge 3.5.1)', $ret);
-
-// -----------------------------------------------------------------------------------
-// XBD 1.1 tests.
-// -----------------------------------------------------------------------------------
-
-$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5';
-
-$ret = _browser();
-_print('Mac useragent', $ret);
-
-// -----------------------------------------------------------------------------------
-// XBD 1.0, can return (string) OS X version.
-// Or if using OS X (bool)
-//
-// XBD 1.1, can return an (array) based on first arg.
-// -----------------------------------------------------------------------------------
-$ret = _browser('os x', false, true);
-_print('OS X string', $ret);
-
-$ret = _browser('os x');
-_print('OS X bool', $ret);
-
-$ret = _browser('os x', false, false, false, true);
-_print('Mac OS X array', $ret);
-
-// -----------------------------------------------------------------------------------
-// How to know the Windows version from the client?
-//
-// I found a useful information page from Microsoft explaining the user agent strings here:
-// http://msdn.microsoft.com/en-us/library/ms537503(v=vs.85).aspx
-//
-// So, getting the "Windows NT" part from the useragent, we can known the Windows version :-) 
-// -----------------------------------------------------------------------------------
-
-$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9b2pre) Gecko/2007120505 Minefield/3.0b2pre';
-
-$ret = _browser('windows nt', false, false, false, true);
-
-_print('Windows array', $ret);
-
-$windows_array = array(
-	'6.1' => 'Windows 7',
-	'6.0' => 'Windows Vista',
-	'5.2' => 'Windows Server 2003 or Windows XP x64',
-	'5.1' => 'Windows XP'
-);
-
-if (isset($windows_array[$ret['version']]))
-{
-	_print('Client is using', $windows_array[$ret['version']]);
-}
-else
-{
-	_print('Windows version not found.');
-}
-
-// -----------------------------------------------------------------------------------
-// Detect if Windows version is below or above a determined version.
-// Based on $windows_array you can determine the version number for the condition.
-// -----------------------------------------------------------------------------------
-
-$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9b2pre) Gecko/2007120505 Minefield/3.0b2pre';
-
-$windows_array = array(
-	'6.1' => 'Windows 7',
-	'6.0' => 'Windows Vista',
-	'5.2' => 'Windows Server 2003 or Windows XP x64',
-	'5.1' => 'Windows XP'
-);
-
-$ret = _browser('windows nt', false, false, false, true);
-
-if (isset($ret['version']) && !empty($ret['version']))
-{
-	$detect_by_name = 'Windows Vista';
-	$detect_by_number = array_search($detect_by_name, $windows_array);
-	
-	if ($ret['version'] == $detect_by_number)
-	{
-		_print('Detected version (' . $windows_array[$ret['version']] . ') is equal to ' . $windows_array[$detect_by_number]);
-	}
-	elseif ($ret['version'] <= $detect_by_number)
-	{
-		_print('Detected version ' . $windows_array[$ret['version']] . ' is below to ' . $windows_array[$detect_by_number]);
-	}
-	else
-	{
-		_print('Detected version ' . $windows_array[$ret['version']] . ' is above to ' . $windows_array[$detect_by_number]);
-	}
-}
-else
-{
-	_print('Windows version not found.');
-}
 
 ?>
+<html>
+<head>
+	<title>xbd : extended browser data</title>
+	<style type="text/css">
+	body {
+		background: #000;
+		color: #fff;
+	}
+	.row {
+		width: 80%;
+		margin: 0 auto;
+		clear: both;
+		border: 1px solid #000;
+		height: auto;
+		min-height: 30px;
+	}
+	.col1 {
+		width: 25%;
+		background: #333;
+		padding: 5px;
+		float: left;
+		height: auto;
+		min-height: 20px;		
+	}
+	.col2 {
+		width: 73%;
+		background: #666;
+		padding: 5px;
+		float: left;
+		height: auto;
+		min-height: 20px;		
+	}
+	</style>
+</head>
+<body>
+<?
+//include and init
+require('xbd.php');
+$x = new xbd();
+
+//---current
+$info = $x->browser();
+echo '<div class="row"><h1>your browser</h1></div>';
+echo '<div class="row"><div class="col1">browser</div><div class="col2">'.$info['browser'].'</div></div>';
+echo '<div class="row"><div class="col1">platform</div><div class="col2">'.$info['platform'].'</div></div>';
+echo '<div class="row"><div class="col1">version</div><div class="col2">'.$info['version'].'</div></div>';
+echo '<div class="row"><div class="col1">type</div><div class="col2">'.$info['type'].'</div></div>';
+echo '<div class="row"><div class="col1">useragent</div><div class="col2">'.$info['useragent'].'</div></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake android ua
+$_SERVER["HTTP_USER_AGENT"] = 'mozilla/5.0 (linux; u; android 2.3.7; en-us; desire hd build/gri40) applewebkit/533.1 (khtml, like gecko) version/4.0 mobile safari/533.1';
+$info = $x->browser();
+echo '<div class="row"><h1>android</h1></div>';
+echo '<div class="row"><div class="col1">browser</div><div class="col2">'.$info['browser'].'</div></div>';
+echo '<div class="row"><div class="col1">platform</div><div class="col2">'.$info['platform'].'</div></div>';
+echo '<div class="row"><div class="col1">version</div><div class="col2">'.$info['version'].'</div></div>';
+echo '<div class="row"><div class="col1">type</div><div class="col2">'.$info['type'].'</div></div>';
+echo '<div class="row"><div class="col1">useragent</div><div class="col2">'.$info['useragent'].'</div></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake iphone ua
+$_SERVER["HTTP_USER_AGENT"] = 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1C25 Safari/419.3';
+$info = $x->browser();
+echo '<div class="row"><h1>iphone</h1></div>';
+echo '<div class="row"><div class="col1">browser</div><div class="col2">'.$info['browser'].'</div></div>';
+echo '<div class="row"><div class="col1">platform</div><div class="col2">'.$info['platform'].'</div></div>';
+echo '<div class="row"><div class="col1">version</div><div class="col2">'.$info['version'].'</div></div>';
+echo '<div class="row"><div class="col1">type</div><div class="col2">'.$info['type'].'</div></div>';
+echo '<div class="row"><div class="col1">useragent</div><div class="col2">'.$info['useragent'].'</div></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake chrome ua
+$_SERVER["HTTP_USER_AGENT"] = 'mozilla/5.0 (windows nt 6.1; wow64) applewebkit/535.11 (khtml, like gecko) chrome/17.0.963.79 safari/535.11';
+$info = $x->browser();
+echo '<div class="row"><h1>chrome</h1></div>';
+echo '<div class="row"><div class="col1">browser</div><div class="col2">'.$info['browser'].'</div></div>';
+echo '<div class="row"><div class="col1">platform</div><div class="col2">'.$info['platform'].'</div></div>';
+echo '<div class="row"><div class="col1">version</div><div class="col2">'.$info['version'].'</div></div>';
+echo '<div class="row"><div class="col1">type</div><div class="col2">'.$info['type'].'</div></div>';
+echo '<div class="row"><div class="col1">useragent</div><div class="col2">'.$info['useragent'].'</div></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake ie ua
+$_SERVER["HTTP_USER_AGENT"] = 'mozilla/4.0 (compatible; msie 7.0; windows nt 6.1; wow64; trident/5.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; media center pc 6.0; .net4.0c; .net4.0e)';
+$info = $x->browser();
+echo '<div class="row"><h1>internet explorer</h1></div>';
+echo '<div class="row"><div class="col1">browser</div><div class="col2">'.$info['browser'].'</div></div>';
+echo '<div class="row"><div class="col1">platform</div><div class="col2">'.$info['platform'].'</div></div>';
+echo '<div class="row"><div class="col1">version</div><div class="col2">'.$info['version'].'</div></div>';
+echo '<div class="row"><div class="col1">type</div><div class="col2">'.$info['type'].'</div></div>';
+echo '<div class="row"><div class="col1">useragent</div><div class="col2">'.$info['useragent'].'</div></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake ipv4
+$_SERVER["REMOTE_ADDR"] = '207.97.227.239';
+$info = $x->browser();
+echo '<div class="row"><h1>ip v6</h1></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+//---fake ipv6
+$_SERVER["REMOTE_ADDR"] = '2001:470:0:64::2';
+$info = $x->browser();
+echo '<div class="row"><h1>ip v6</h1></div>';
+echo '<div class="row"><div class="col1">ip</div><div class="col2">'.$info['ipaddress'].'</div></div>';
+echo '<div class="row"><div class="col1">hostname</div><div class="col2">'.$info['hostname'].'</div></div>';
+echo '<br/><br/>';
+
+
+?>
+</body>
+</html>
